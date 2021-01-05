bedd024af3c906e8cbba33a0ff68a669f56ec987
Bitvinus <dezenu@ya.ru>
2021-01-05 12:56:58 +0300

    sort added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const obj2 = {
 const diffGen = (obj1, obj2) => {
     const keys1 = Object.keys(obj1);
     const keys2 = Object.keys(obj2);
-    const allKeys = _.union(keys1, keys2);
+    const allKeys = _.union(keys1, keys2).sort();
     const result = {};
 
     for (const key of allKeys) {
